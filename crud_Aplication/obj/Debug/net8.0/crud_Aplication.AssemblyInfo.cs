f55f049fbe810fb399d372a8a00f561c14a52bee
mrZabala <peliezer51@gmail.com>
2025-09-21 23:45:35 -0400

    Eto lo que dio brega fue

--- crud_Aplication/obj/Debug/net8.0/crud_Aplication.AssemblyInfo.cs
+++ crud_Aplication/obj/Debug/net8.0/crud_Aplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("crud_Aplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd513ba507e2f21ffaf3a2ae2cba2f092580dec")]
 [assembly: System.Reflection.AssemblyProductAttribute("crud_Aplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("crud_Aplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
