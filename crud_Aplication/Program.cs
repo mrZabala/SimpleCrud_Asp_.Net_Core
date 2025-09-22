6d56f1dd623153e6501765d456d120315b217111
mrZabala <peliezer51@gmail.com>
2025-09-21 23:37:31 -0400

    llamando contexto de la BD desde el InitController y creando los metodos de obtenr datos desde la bd de manera asincrona

--- crud_Aplication/Program.cs
+++ crud_Aplication/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Init}/{action=Index}/{id?}");
 
 app.Run();
