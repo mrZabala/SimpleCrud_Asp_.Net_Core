6dd513ba507e2f21ffaf3a2ae2cba2f092580dec
mrZabala <peliezer51@gmail.com>
2025-09-21 23:17:38 -0400

    Commit inicial

---
+++ crud_Aplication/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using crud_Aplication.Models;
+
+namespace crud_Aplication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<crud_Aplication.Models.Contact> Contacts { get; set; }
+    }
+}
---
+++ crud_Aplication/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crud_Aplication.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
